6a09f787847eeb47f8a3b20c906f320d0f97441f
jazad136 <1129384+jazad136@users.noreply.github.com>
2026-07-03 15:47:46 -0400

    Use random number 1-10

---
+++ functions/LuckynumgenSingle1x/src/main/java/com/jschway/luckynumgen/HandleOne.java
@@ -0,0 +1,34 @@
+package com.jschway.luckynumgen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class HandleOne implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        Random r = new Random();
+        int numberOut = r.nextInt(0,10);
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+       
+        String output = String.format("{ \"message\": \"Lucky Number\", \"number\": \"%s\" }", numberOut);
+        return response
+                .withStatusCode(200)
+                .withBody(output);
+    }
+}
