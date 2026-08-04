267b5e76a7ed1d9dc0c2946412c6af906db802c5
jazad136 <1129384+jazad136@users.noreply.github.com>
2026-08-03 20:28:41 -0400

    Begin to implement Selenium testing

--- test/luckynumgen-single-tests/src/main/java/com/jschway/luckynumgen/Handle.java
+++ test/luckynumgen-single-tests/src/main/java/com/jschway/luckynumgen/Handle.java
@@ -1,18 +1,38 @@
 package com.jschway.luckynumgen;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class Handle {
-    public static String generateOne(String input) { 
+    public Handle() { 
+        
+    }
+    public List<String> generateThree(String numberIn) { 
+        List<String> previous = new LinkedList<>();
+        previous.add(newLuckyNumber(numberIn));
+        previous.add(newLuckyNumber(numberIn, previous));
+        previous.add(newLuckyNumber(numberIn, previous));
+        return previous;
+    }
+    public String newLuckyNumber(String numberIn) { 
         Random r = new Random();
-        int numberOut = r.nextInt(0,10);
+        int randomPart = r.nextInt(0,9)+1;
         int position = r.nextInt(2);
-        String stringOut = "" + numberOut;
+        String stringOut = "" + randomPart;
         if(position == 0) { 
-            stringOut = input + stringOut;
+            stringOut = numberIn + stringOut;
         }
         else 
-            stringOut = stringOut + input;
+            stringOut = stringOut + numberIn;
+        return stringOut;
+    }
+    
+    public String newLuckyNumber(String numberIn, List<String> previous) { 
+        String stringOut = "";
+        do { 
+            stringOut = newLuckyNumber(numberIn);
+        } while(previous.contains(stringOut)); // do not regenerate a number
         return stringOut;
     }
 }
