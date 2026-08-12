db55a1804b2d7bc076efdf8bcf284b8145f99efe
jazad136 <1129384+jazad136@users.noreply.github.com>
2026-08-12 16:43:38 -0400

    Add a rest tests package for local rest api testing

---
+++ test/luckynumgen-single-rest-tests/src/test/java/com/jschway/luckynumgen/tests/rest/PositiveNumberTest1x.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jschway.luckynumgen.tests.rest;
+
+import com.jschway.luckynumgen.tests.TestBase;
+import com.jschway.luckynumgen.tests.config.TestDataProvider;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import org.testng.ITestResult;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author JonathanSaddler
+ */
+public class PositiveNumberTest1x extends TestBase{
+    @BeforeClass(dependsOnMethods = "init")
+    public void setBasePath() { 
+        RestAssured.basePath = "/onex";
+    }
+    
+    @Test(dataProviderClass = TestDataProvider.class, 
+          dataProvider = "methodDataProviderLogicSuite")
+    public void testGenContainsNumber(String numberIn, String numberContains, String testName) {
+        i("Testing response is 200");
+        var response = given()
+         .pathParam("numberIn", numberIn)
+         .when()
+         .get("/onex/{numberIn}")
+         .then()
+         .statusCode(200);
+        i("Testing number1 contains " + numberContains);
+        response
+         .and()
+         .body("number1", (resp) -> contains(numberContains));
+        i("Testing number2 contains " + numberContains);
+        response
+         .and()
+         .body("number2", (resp) -> contains(numberContains));
+        i("Testing number3 contains " + numberContains);
+        response
+         .and()
+         .body("number3", (resp) -> contains(numberContains));
+    }
+
+//    @DisplayName("Get the total value from the response")
+//    @Test
+//    public void getTotalFromResponse() {
+//        int totalValue = JsonPath.read(jsonResponse, "$.total");
+//        print(totalValue + "");
+//    }
+    
+//    @DisplayName("Get all the data elements")
+//    @Test
+//    public void getAllDataElements() { 
+//        List<HashMap<String, Object>> dataElements = JsonPath.read(jsonResponse, "$.data");
+//        dataElements.stream().forEach(System.out::println);
+//    }
+//    
+//    @DisplayName("Get firstDataElement")
+//    @Test
+//    public void getFirstDataElement() { 
+//        Map<String,?> firstDataElement = JsonPath.read(jsonResponse, "$.data[0]");
+//        print(firstDataElement.toString());
+//    }
+//    @DisplayName("Get lastDataElement")
+//    @Test
+//    public void getLastDataElement() { 
+//        Map<String,?> firstDataElement = JsonPath.read(jsonResponse, "$.data[-1]");
+//        print(firstDataElement.toString());
+//    }
+//    
+//    @DisplayName("Get all ids in the data")
+//    @Test
+//    public void getAllIdsUnderData() { 
+//        List<String> dataElements = JsonPath.read(jsonResponse, "$.data[*].id");
+//        print(dataElements.toString());
+//    }
+//    
+//    @DisplayName("Get all ids in the data")
+//    @Test
+//    public void getAllIds() { 
+//        List<String> dataElements = JsonPath.read(jsonResponse, "$..id");
+//        print(dataElements.toString());
+//    }
+
+    @Override
+    public String retrieveTestNameSuffix(ITestResult res) {
+        return TestDataProvider.getTestNamePositiveSuite(
+                res.getMethod().getMethodName(), 
+                res.getParameters()
+        );
+    }
+    
+    
+}
