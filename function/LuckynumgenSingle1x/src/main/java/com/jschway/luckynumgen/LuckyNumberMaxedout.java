46db1c84c06c7754e2a32b46647b0cbe91a51d82
jazad136 <1129384+jazad136@users.noreply.github.com>
2026-08-17 05:52:19 -0400

    [EXCEPT] Simplify maxedout exception calls

--- function/LuckynumgenSingle1x/src/main/java/com/jschway/luckynumgen/LuckyNumberMaxedout.java
+++ function/LuckynumgenSingle1x/src/main/java/com/jschway/luckynumgen/LuckyNumberMaxedout.java
@@ -2,10 +2,16 @@ package com.jschway.luckynumgen;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 
-public class LuckyNumberMaxedout extends LuckyNumberMessage { 
+public class LuckyNumberMaxedout extends LuckyNumbersResponseType { 
     
-    public LuckyNumberMaxedout(String message) { super(message); }
+    public LuckyNumberMaxedout(String message) { 
+        setMessage(message);
+        String[] split3 = message.split(" ");
+        setNumber1(split3[0]);
+        setNumber2(split3[1]);
+        setNumber3(split3[2]);
+    }
     
     @JsonGetter("type")
-    public String getType() { return "error"; }
+    public String getType() { return "exception"; }
 }
